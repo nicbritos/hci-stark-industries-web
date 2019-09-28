fc3fca87b9783a51ea49305d599e1e27847c90ca
Lucas Dell'Isola <ldellisola@itba.edu.ar>
2019-09-28 15:45:36 -0300

    Revert "Revert "Merge branch 'lucas/Curtains-menu'""
    This reverts commit ddc4a70cf720517e659a69a3690fdb921aaed1dc.

---
+++ src/components/Menus/CurtainsMenu.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-dialog v-model="CurtainsMenu" persistent max-width="400px">
+        <template v-slot:activator="{ on }">
+            <v-btn color="primary" dark v-on="on">Open Curtain</v-btn>
+        </template>
+
+
+    <v-card dark raised>
+        <v-card-title>
+            <span class="headline">Curtains</span>
+
+
+            <v-btn icon absolute right @click="CurtainsMenu = false">
+                <v-avatar color="red">
+                    <v-icon>delete</v-icon>
+
+                </v-avatar>
+            </v-btn>
+
+        </v-card-title>
+        <v-card-text>
+            <v-container >
+                <v-row justify="span">
+                    <v-col>
+                        <img id="image" src="../../assets/CurtainOpen.svg"/>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-row>
+                            <v-btn width="50" height="60"  color="blue">
+                                <v-img  src="../../assets/arrowUp.svg"/>
+                            </v-btn>
+                        </v-row>
+                        <v-row>
+                            <br/>
+                        </v-row>
+                        <v-row>
+                            <v-btn width="50" height="60" color="blue">
+                                <v-img  style="rotation:180deg " src="../../assets/arrowDown.svg"/>
+                            </v-btn>
+                        </v-row>
+                    </v-col>
+                </v-row>
+
+            </v-container>
+        </v-card-text>
+        <v-card-actions class="justify-center" >
+            <div class="text-center">
+                <v-btn color="red"  @click="CurtainsMenu = false">Cancel</v-btn>
+                <v-btn color="blue"  @click="CurtainsMenu = false">Confirm</v-btn>
+            </div>
+        </v-card-actions>
+    </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    export default {
+        name: "CurtainsMenu",
+        data: () => ({
+            CurtainsMenu: false,
+        }),
+        methods:{
+
+        }
+
+    }
+
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,9 @@
         <v-divider></v-divider>
       </v-col>
     </v-row>
+    <v-row>
+      
+    </v-row>
 
     <v-row>
       <v-col>
@@ -32,10 +35,11 @@
 <script>
 import RoutineContainer from "@/components/containers/RoutineContainer";
 import DeviceContainer from "@/components/containers/DeviceContainer";
+import CurtainsMenu from "../components/Menus/CurtainsMenu";
 
 export default {
   name: "Home",
-  components: { RoutineContainer, DeviceContainer },
+  components: { CurtainsMenu,RoutineContainer, DeviceContainer },
   data() {
     return {
       devices: this.$store.state.devices.favourites,
