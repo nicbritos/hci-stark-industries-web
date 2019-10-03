d893ad0f20311f28cfba77ad37396af1a88bb214
Nico <nicbritos@hotmail.com>
2019-10-03 09:02:26 -0300

    Created BoxContainer

---
+++ src/components/containers/BoxContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container fluid>
+    <v-row justify="start">
+      <v-col cols="auto" v-for="item in items" :key="item.id">
+        <slot name="item" v-bind:item="item"></slot>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "BoxContainer",
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped></style>
