f893b57c88aa4f0eb3c3daa7cec6275bdbbbc053
Lucas Dell'Isola <ldellisola@itba.edu.ar>
2019-10-04 15:49:24 -0300

    Implemented basic  API usage for door and curtains

--- src/components/Menus/CurtainsMenu.vue
+++ src/components/Menus/CurtainsMenu.vue
@@ -1,108 +1,209 @@
 <template>
-    <v-dialog v-model="CurtainsMenu" persistent max-width="400px">
-        <template v-slot:activator="{ on }">
-            <v-btn color="primary" dark v-on="on">Open Curtain</v-btn>
-        </template>
-
+  <v-dialog v-model="CurtainsMenu" persistent max-width="400px">
+    <template v-slot:activator="{ on }">
+      <v-btn color="primary" dark v-on="on">Open Curtain</v-btn>
+    </template>
 
     <v-card dark raised>
-        <v-card-title>
-            <span class="headline">{{name}}</span>
+      <v-card-title>
+        <span class="headline">{{ name }}</span>
 
-
-            <v-btn icon absolute right @click="CurtainsMenu = false">
-                <v-avatar color="red">
-                    <v-icon>delete</v-icon>
-
-                </v-avatar>
-            </v-btn>
-
-        </v-card-title>
-        <v-card-text>
-            <v-container >
-                <v-row justify="space-between">
-                    <v-col>
-                        <img id="image" :src="ImageCurrent"/>
-                    </v-col>
-                    <v-col cols="4">
-                        <v-row>
-                            <v-btn :disabled="this.ButtonUp.disabled" width="50" height="60"  color="blue" @click="OpenCurtain()">
-                                <v-icon>
-                                    keyboard_arrow_up
-                                </v-icon>
-                            </v-btn>
-                        </v-row>
-                        <v-row>
-                            <br/>
-                        </v-row>
-                        <v-row>
-                            <v-btn :disabled="this.ButtonDown.disabled" width="50" height="60" color="blue" @click="CloseCurtain()">
-                                <v-icon>
-                                    keyboard_arrow_down
-                                </v-icon>
-                            </v-btn>
-                        </v-row>
-                    </v-col>
-                </v-row>
-
-            </v-container>
-        </v-card-text>
-        <v-card-actions class="justify-center" >
-            <div class="text-center">
-                <v-btn color="red"  @click="CurtainsMenu = false">Cancel</v-btn>
-                <v-btn color="blue"  @click="CurtainsMenu = false">Confirm</v-btn>
-            </div>
-        </v-card-actions>
+        <v-btn icon absolute right @click="Delete()">
+          <v-avatar color="red">
+            <v-icon>delete</v-icon>
+          </v-avatar>
+        </v-btn>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row justify="space-between">
+            <v-col>
+              <img id="image" :src="ImageCurrent" />
+            </v-col>
+            <v-col cols="4">
+              <v-row>
+                <v-btn
+                  :disabled="this.ButtonUp.disabled"
+                  width="50"
+                  height="60"
+                  color="blue"
+                  @click="OpenCurtain()"
+                >
+                  <v-icon>
+                    keyboard_arrow_up
+                  </v-icon>
+                </v-btn>
+              </v-row>
+              <v-row>
+                <br />
+              </v-row>
+              <v-row>
+                <v-btn
+                  :disabled="this.ButtonDown.disabled"
+                  width="50"
+                  height="60"
+                  color="blue"
+                  @click="CloseCurtain()"
+                >
+                  <v-icon>
+                    keyboard_arrow_down
+                  </v-icon>
+                </v-btn>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions class="justify-center">
+        <div class="text-center">
+          <v-btn color="red" @click="Exit()">Cancel</v-btn>
+          <v-btn color="blue" @click="SaveAndExit()">Confirm</v-btn>
+        </div>
+      </v-card-actions>
     </v-card>
-    </v-dialog>
+  </v-dialog>
 </template>
 
 <script>
-    export default {
-        name: "curtainsMenu",
-        props:[
-            'name'
-        ],
-        data: () => ({
+export default {
+  name: "curtainsMenu",
+  props: {
+    deviceId: {
+      type: String,
+      required: false
+    },
+    mode: {
+      type: String,
+      required: true
+    },
+      name:{
+        type: String,
+        required: true
+      }
+  },
+  data: () => ({
+    ButtonUp: {
+      disabled: true,
+      ImageToSet: "img/devices/CurtainOpen.svg"
+    },
 
-            ButtonUp:{
-                disabled:true,ImageToSet:"img/devices/CurtainOpen.svg"
-            },
+    ButtonDown: {
+      disabled: false,
+      ImageToSet: "img/devices/CurtainClosed.svg"
+    },
 
-            ButtonDown:{
-                disabled:false,ImageToSet:"img/devices/CurtainClosed.svg"
-            },
+    CurtainsMenu: false,
+    ImageCurrent: "",
+  }),
+  methods: {
+      Delete(){
+          var myInit = {
+              method: "DELETE",
+              headers: { "Content-Type": "application/json" }
+          };
+          fetch(
+              `http://localhost:8080/api/devices/${this.deviceId}`,
+              myInit
+          )
+              .then(response => {
+                  console.log("Request recibido");
+                  return response.json();
+              })
+              .catch(error => {
+                  console.log(error);
+              });
 
-            CurtainsMenu: false,
-            ImageCurrent:"",
-        }),
-        methods:{
-            OpenCurtain(){
+          this.Exit();
+      },
+    SaveAndExit() {
+      var myInit = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" }
+      };
 
-                this.ButtonUp.disabled=true;
-                this.ButtonDown.disabled=false;
-                this.ImageCurrent= this.ButtonUp.ImageToSet;
+      if (this.ButtonDown.disabled === true) {
+        fetch(
+          `http://localhost:8080/api/devices/${this.deviceId}/close`,
+          myInit
+        )
+          .then(response => {
+            console.log("Request recibido");
+            return response.json();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
+      else if(this.ButtonUp.disabled === true) {
+        fetch(
+          `http://localhost:8080/api/devices/${this.deviceId}/open`,
+          myInit
+        )
+          .then(response => {
+            console.log("Request recibido");
+            return response.json();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
 
-            },
-            CloseCurtain(){
-                this.ButtonUp.disabled=false;
-                this.ButtonDown.disabled=true;
-                this.ImageCurrent= this.ButtonDown.ImageToSet;
+      this.Exit();
 
-            },
-            SetUp(){
-                this.ImageCurrent=this.ButtonUp.ImageToSet;
-            }
-        },
-        mounted(){
-            this.SetUp()
-        },
+    },
+      Exit(){
+          this.CurtainsMenu = false;
+      },
+    OpenCurtain() {
+      this.ButtonUp.disabled = true;
+      this.ButtonDown.disabled = false;
+      this.ImageCurrent = this.ButtonUp.ImageToSet;
+    },
+    CloseCurtain() {
+      this.ButtonUp.disabled = false;
+      this.ButtonDown.disabled = true;
+      this.ImageCurrent = this.ButtonDown.ImageToSet;
+    },
+    SetUp() {
+      this.ImageCurrent = this.ButtonUp.ImageToSet;
+    },
+    LoadModel(model) {
+      console.log(model);
+      if (model.status === "opened" || model.status === "opening") {
+        this.OpenCurtain();
+      } else if (model.status === "closed" || model.status === "closing") {
+        this.CloseCurtain();
+      }
+    },
+    SetUpForEdit(id) {
+      console.log("Haciendo el request");
+      var myInit = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" }
+      };
+
+      fetch(`http://localhost:8080/api/devices/${id}/getState`, myInit)
+        .then(response => {
+          console.log("Request recibido");
+          return response.json();
+        })
+        .then(data => {
+          this.LoadModel(data.result);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
-
-
-
+  },
+  watch: {
+    CurtainsMenu: function(val) {
+      if (this.mode === "edit" && val) {
+        console.log("entrando al modo editar");
+        this.SetUpForEdit(this.deviceId);
+      }
+    }
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Menus/DoorMenu.vue
+++ src/components/Menus/DoorMenu.vue
@@ -8,7 +8,7 @@
       <v-card-title>
         <span class="headline">{{ name }}</span>
 
-        <v-btn icon absolute right @click="DoorMenu = false">
+        <v-btn icon absolute right @click="DeleteAndExit()">
           <v-avatar color="red">
             <v-icon>delete</v-icon>
           </v-avatar>
@@ -17,51 +17,49 @@
       <v-card-text clas="text-center">
         <v-container>
           <v-row>
-              <v-col>
+            <v-col>
+              <v-row class=" mb-6 " justify="center" no-gutters>
+                <v-avatar color="green" size="50">
+                  <v-img :src="this.SecurityRepresentation.image" />
+                </v-avatar>
+              </v-row>
 
-                <v-row class=" mb-6 " justify="center"  no-gutters>
-                    <v-avatar color="green" size="50">
-                      <v-img :src="currentSecurityImage" />
-                    </v-avatar>
-                </v-row>
+              <v-row class=" mb-6 " justify="center" no-gutters>
+                <v-img
+                  max-width="100"
+                  max-height="300"
+                  :src="this.StatusRepresentation.image"
+                />
+              </v-row>
+            </v-col>
+            <v-col>
+              <div class="buttonsContainer">
+                <v-btn
+                  color="blue"
+                  class="text-center"
+                  :disabled="!this.StatusRepresentation.btnEnabled"
+                  @click="ChangeDoorState()"
+                >
+                  {{ this.StatusRepresentation.btnString }}
+                </v-btn>
 
-                <v-row class=" mb-6 " justify="center" no-gutters>
-                  <v-img
-                    max-width="100"
-                    max-height="300"
-                    :src="CurrentDoorImage"
-                  />
-                </v-row>
-
-              </v-col>
-            <v-col >
-                <div class="buttonsContainer">
-                  <v-btn
-                    color="blue"
-                    class="text-center"
-                    :disabled="!enableOpenCloseButton"
-                    @click="ChangeDoorState()"
-                  >
-                    {{ this.currentOpenCloseButtonString }}
-                  </v-btn>
-
-                  <v-btn
-                    color="blue"
-                    class="text-center"
-                    :disabled="!enableLockUnlockButton"
-                    @click="ChangeSecurityState()"
-                  >
-                    {{ this.currentLockUnlockButtonString }}
-                  </v-btn>
-                </div>
+                <v-btn
+                  color="blue"
+                  class="text-center"
+                  :disabled="!this.SecurityRepresentation.btnEnabled"
+                  @click="ChangeSecurityState()"
+                >
+                  {{ this.SecurityRepresentation.btnString }}
+                </v-btn>
+              </div>
             </v-col>
           </v-row>
         </v-container>
       </v-card-text>
       <v-card-actions class="justify-center">
         <div class="text-center">
-          <v-btn color="red" @click="DoorMenu = false">Cancel</v-btn>
-          <v-btn color="blue" @click="DoorMenu = false">Confirm</v-btn>
+          <v-btn color="red" @click="Exit()">Cancel</v-btn>
+          <v-btn color="blue" @click="SaveAndExit()">Confirm</v-btn>
         </div>
       </v-card-actions>
     </v-card>
@@ -72,81 +70,219 @@
 export default {
   name: "DoorMenu",
 
-  props: ["name"],
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    deviceId: {
+      type: String,
+      required: true
+    }
+  },
 
   data: () => ({
     DoorMenu: false,
 
+    State: {
+      open: true,
+      locked: false
+    },
+
+    SecurityRepresentation: {
+      btnString: "asda",
+      btnEnabled: false,
+      image: "asdasd"
+    },
+
+    StatusRepresentation: {
+      btnString: "asdasd",
+      btnEnabled: true,
+      image: "dasd"
+    },
+
     closedDoorImage: "img/devices/ClosedDoor.svg",
     openDoorImage: "img/devices/OpenDoor.svg",
+
     lockDoorImage: "img/devices/DoorLock.svg",
     unlockDoorImage: "img/devices/DoorUnlock.svg",
-
-    currentSecurityImage: "img/devices/DoorUnlock.svg",
-    CurrentDoorImage: "img/devices/OpenDoor.svg",
 
     OpenButtonString: "Open Door",
     CloseButtonString: "Close Door",
 
-    currentOpenCloseButtonString: "Close Door",
-
     LockButtonString: "Lock Door",
-    UnlockButtonString: "Unlock Door",
-
-    enableOpenCloseButton: true,
-    enableLockUnlockButton: false,
-
-    currentLockUnlockButtonString: "Lock Door"
+    UnlockButtonString: "Unlock Door"
   }),
   methods: {
     ChangeDoorState() {
-      if (this.currentOpenCloseButtonString === this.OpenButtonString) {
+      if (!this.State.open) {
         this.SetOpenState();
-      } else if (this.currentOpenCloseButtonString === this.CloseButtonString) {
+      } else if (this.State.open) {
         this.SetCloseState();
       }
     },
     SetOpenState() {
-      this.CurrentDoorImage = this.openDoorImage;
-      this.currentOpenCloseButtonString = this.CloseButtonString;
-      this.enableLockUnlockButton = false;
+      this.StatusRepresentation.image = this.openDoorImage;
+      this.StatusRepresentation.btnString = this.CloseButtonString;
+      this.SecurityRepresentation.btnEnabled = false;
+      this.State.open = true;
     },
     SetCloseState() {
-      this.CurrentDoorImage = this.closedDoorImage;
-      this.currentOpenCloseButtonString = this.OpenButtonString;
-      this.enableLockUnlockButton = true;
+      this.StatusRepresentation.image = this.closedDoorImage;
+      this.StatusRepresentation.btnString = this.OpenButtonString;
+      this.SecurityRepresentation.btnEnabled = true;
+      this.State.open = false;
     },
     ChangeSecurityState() {
-      if (this.currentLockUnlockButtonString === this.LockButtonString) {
+      if (!this.State.locked) {
         this.SetLockState();
-      } else if (
-        this.currentLockUnlockButtonString === this.UnlockButtonString
-      ) {
+      } else if (this.State.locked) {
         this.SetUnlockState();
       }
     },
     SetUnlockState() {
-      this.enableOpenCloseButton = true;
-      this.currentLockUnlockButtonString = this.LockButtonString;
-      this.currentSecurityImage = this.unlockDoorImage;
+      this.StatusRepresentation.btnEnabled = true;
+      this.SecurityRepresentation.btnString = this.LockButtonString;
+      this.SecurityRepresentation.image = this.unlockDoorImage;
+      this.State.locked = false;
     },
     SetLockState() {
-      this.enableOpenCloseButton = false;
-      this.currentLockUnlockButtonString = this.UnlockButtonString;
-      this.currentSecurityImage = this.lockDoorImage;
+      this.StatusRepresentation.btnEnabled = false;
+      this.SecurityRepresentation.btnString = this.UnlockButtonString;
+      this.SecurityRepresentation.image = this.lockDoorImage;
+      this.State.locked = true;
+    },
+    LoadModel(model) {
+      if (model.status === "opened") {
+        this.SetOpenState();
+      } else {
+        this.SetCloseState();
+      }
 
+      if (model.lock === "locked") {
+        this.SetLockState();
+      } else {
+        this.SetUnlockState();
+      }
+    },
+    SetUpForEdit(id) {
+      var myInit = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" }
+      };
+      fetch(`http://localhost:8080/api/devices/${id}/getState`, myInit)
+        .then(response => {
+          console.log("Request recibido");
+          return response.json();
+        })
+        .then(data => this.LoadModel(data.result))
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    SaveAndExit() {
+      var myInit = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" }
+      };
+      if (this.State.open) {
+        fetch(
+          `http://localhost:8080/api/devices/${this.deviceId}/unlock`,
+          myInit
+        )
+          .then(response => {
+            console.log("Request recibido");
+            return response.json();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+
+        fetch(`http://localhost:8080/api/devices/${this.deviceId}/open`, myInit)
+          .then(response => {
+            console.log("Request recibido");
+            return response.json();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      } else {
+        fetch(
+          `http://localhost:8080/api/devices/${this.deviceId}/close`,
+          myInit
+        )
+          .then(response => {
+            console.log("Request recibido");
+            return response.json();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        if (this.State.locked) {
+          fetch(
+            `http://localhost:8080/api/devices/${this.deviceId}/lock`,
+            myInit
+          )
+            .then(response => {
+              console.log("Request recibido");
+              return response.json();
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        } else {
+          fetch(
+            `http://localhost:8080/api/devices/${this.deviceId}/unlock`,
+            myInit
+          )
+            .then(response => {
+              console.log("Request recibido");
+              return response.json();
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        }
+      }
+
+      this.Exit();
+    },
+    Exit() {
+      this.DoorMenu = false;
+    },
+    DeleteAndExit() {
+      var myInit = {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" }
+      };
+      fetch(`http://localhost:8080/api/devices/${this.deviceId}`, myInit)
+        .then(response => {
+          console.log("Request recibido");
+          return response.json();
+        })
+        .then(data => this.LoadModel(data.result))
+        .catch(error => {
+          console.log(error);
+        });
+
+      this.Exit();
     }
-  }
+  },
+  watch: {
+    DoorMenu: function(val) {
+      if (val) {
+        this.SetUpForEdit(this.deviceId);
+      }
+    }
+  },
 };
 </script>
 
 <style scoped>
-
-    .buttonsContainer{
-        display: flex;
-        justify-content: space-between;
-        text-align: center;
-        align-content: space-between;
-    }
-
+.buttonsContainer {
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  align-content: space-between;
+}
 </style>
