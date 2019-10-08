4c830c143c140890a29e48afd69eb6f824da89f1
juan <olivajuan12@gmailc>
2019-10-08 17:48:40 -0300

    wrappers de AC, Lamp y Oven

--- src/components/Menus/LampMenu.vue
+++ src/components/Menus/LampMenu.vue
@@ -4,7 +4,7 @@
     <v-card dark raised>
       <v-card-title>
         <span class="headline">{{ name }}</span>
-        <v-btn icon absolute right @click="Exit()">
+        <v-btn icon absolute right @click="Delete()">
           <v-avatar color="red">
             <v-icon>delete</v-icon>
           </v-avatar>
@@ -33,14 +33,19 @@
             <span>Color</span>
           </v-row>
           <v-row class="justify-center">
-            <v-color-picker light v-model="color" hide-inputs></v-color-picker>
+            <v-color-picker
+              light
+              v-model="color"
+              mode="hex"
+              hide-inputs
+            ></v-color-picker>
           </v-row>
         </v-container>
       </v-card-text>
       <v-card-actions class="justify-center">
         <div class="text-center">
           <v-btn color="red" @click="Exit()">Cancel</v-btn>
-          <v-btn color="blue" @click="Exit()">Confirm</v-btn>
+          <v-btn color="blue" @click="SaveAndExit()">Confirm</v-btn>
         </div>
       </v-card-actions>
     </v-card>
@@ -48,6 +53,7 @@
 </template>
 
 <script>
+import Lamp from "../../data/schemas/devices/Lamp";
 export default {
   name: "LampMenu",
   props: {
@@ -65,16 +71,55 @@ export default {
     }
 
   },
-  data() {
-    return {
-      color: "",
-      enabled: false,
-      intensity: 0,
-      SuperMenuOpen:false
-    };
-  },
+  data: () => ({
+    color: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    enabled: false,
+    intensity: 0,
+    SuperMenuOpen: false
+  }),
   methods:{
+    Delete(){
+        console.log("Entrando a DELETE");
+        var APILamp = new Lamp(this.deviceId,this.name);
 
+
+        APILamp.delete();
+
+        this.Exit();
+      },
+    async LoadModel(){
+            let APILamp = new Lamp(this.deviceId, this.name);
+            await APILamp.refreshState();
+
+
+            this.color = APILamp.color;
+            this.enabled = APILamp.isOn;
+            this.intensity = APILamp.brightness;
+        },
+      async SaveAndExit(){
+        var APILamp = new Lamp(this.deviceId, this.name);
+        await APILamp.refreshState();
+        if (APILamp.isOn()) {
+          if (this.enabled) {
+            APILamp.setColor(this.color.r, this.color.g, this.color.b);
+            APILamp.setBrightness(this.intensity);
+          } else {
+            APILamp.turnOff();
+          }
+        } else {
+          if (this.enabled) {
+           APILamp.turnOn();
+           APILamp.setColor(this.color.r, this.color.g, this.color.b);
+           APILamp.setBrightness(this.intensity);
+          }
+      }
+
+      this.Exit();
+        },
     Exit(){
       console.log("Sending Close Event from Lamp")
       this.$emit('CloseMenu')
@@ -82,10 +127,13 @@ export default {
 
   },
   watch:{
-    openMenu:function (val) {
-      console.log("SADASDA")
-      this.SuperMenuOpen= val;
-    }
+     openMenu:function (val) {
+          this.SuperMenuOpen= val;
+          if(val){
+
+              this.LoadModel();
+          }
+      },
   }
 };
 </script>
