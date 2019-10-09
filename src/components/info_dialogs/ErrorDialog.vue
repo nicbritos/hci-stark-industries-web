e81a9f176dcb465e6682cff3b84362b09fae6874
nicbritos <nicbritos@hotmail.com>
2019-10-08 23:02:04 -0300

    Moved and refactored Dialogs

---
+++ src/components/info_dialogs/ErrorDialog.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-dialog v-model="errorDialog" persistent max-width="400px">
+    <template v-slot:activator="{ on }">
+      <v-btn color="primary" dark v-on="on">Error</v-btn>
+    </template>
+    <v-card dark raised>
+      <v-card-title>
+        <span class="headline">{{ name }}</span>
+      </v-card-title>
+      <v-card-text>
+        <v-row align="center">
+          <v-col cols="12" md="2">
+            <v-icon color="red" x-large>error</v-icon>
+          </v-col>
+          <v-col md="10">
+            <span class="subtitle-1">{{ message }}</span>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions class="justify-center">
+        <div class="text-center">
+          <v-btn color="blue" @click="errorDialog = false">OK</v-btn>
+        </div>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "ErrorDialog",
+  props: ["name", "message"],
+  data() {
+    return {
+      errorDialog: false
+    };
+  }
+};
+</script>
+
+<style scoped></style>
