fc870382344aca49e9211fc3986387026cf65451
nicbritos <nicbritos@hotmail.com>
2019-10-09 00:20:44 -0300

    Implemented New/Edit Room dialog

---
+++ src/components/action_menus/rooms/EditRoom.vue
@@ -0,0 +1,40 @@
+<template>
+  <NewEditRoom
+    :regions="regions"
+    :show="show"
+    type="Edit"
+    @closeClick="onClose"
+  ></NewEditRoom>
+</template>
+
+<script>
+import NewEditRoom from "./NewEditRoom";
+import Room from "../../../data/schemas/Room";
+
+export default {
+  name: "NewRoom",
+  components: { NewEditRoom },
+  events: ["closeClick"],
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    },
+    regions: {
+      type: Array,
+      required: true
+    },
+    room: {
+      type: Room,
+      required: true
+    }
+  },
+  methods: {
+    onClose(data) {
+      this.$emit("closeClick", data);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/action_menus/rooms/NewRoom.vue
@@ -0,0 +1,35 @@
+<template>
+  <NewEditRoom
+    :regions="regions"
+    :show="show"
+    type="New"
+    @closeClick="onClose"
+  ></NewEditRoom>
+</template>
+
+<script>
+import NewEditRoom from "./NewEditRoom";
+
+export default {
+  name: "NewRoom",
+  components: { NewEditRoom },
+  events: ["closeClick"],
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    },
+    regions: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    onClose(data) {
+      this.$emit("closeClick", data);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
